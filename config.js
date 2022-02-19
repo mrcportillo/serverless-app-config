44b8b4a6da4d309fd6243038afdca4eb9870ed4a
mrcportillo <mrcportillo@users.noreply.github.com>
2022-02-19 16:14:07 -0300

    adde appconfig api call

--- config.js
+++ config.js
@@ -1,15 +1,37 @@
-"use strict";
+import fetch from "node-fetch";
+
+const APP_CONFIG_LAYER_HOST = "localhost:2772";
+const APP_CONFIG_APPLICATION_ID = "jj2wbav";
+const APP_CONFIG_ENVIRONMENT_ID = "08o0gsc";
+const AOO_CONFIG_CONFIG_ID = "2wa5i6i";
+
+const configUrl = `http://${APP_CONFIG_LAYER_HOST}/applications/${APP_CONFIG_APPLICATION_ID}/environments/${APP_CONFIG_ENVIRONMENT_ID}/configurations/${AOO_CONFIG_CONFIG_ID}`;
 
 module.exports.main = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v2.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const rq = await fetch(configUrl);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v2.0! Your function executed successfully!",
+          config: rq,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: "Something went worng",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  }
 };
